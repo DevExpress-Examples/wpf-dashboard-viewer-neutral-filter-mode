5a967a390a48839e98d9b5463925f520e7beb46f
DevExpressExampleBot <fakeEmail>
2021-12-08 15:03:30 +0300

    Source auto update [skip ci]

--- VB/Wpf_NeutralFilterMode_Example/Wpf_NeutralFilterMode_Example/Dashboard1.vb
+++ VB/Wpf_NeutralFilterMode_Example/Wpf_NeutralFilterMode_Example/Dashboard1.vb
@@ -1,17 +1,12 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-Imports DevExpress.DataAccess
 Imports DevExpress.DashboardCommon
 
 Namespace Wpf_NeutralFilterMode_Example
-	Partial Public Class Dashboard1
-		Inherits DevExpress.DashboardCommon.Dashboard
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class Dashboard1
+        Inherits Dashboard
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
 End Namespace
